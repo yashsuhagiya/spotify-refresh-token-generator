93149dc12f49675f0d32a3fbb6804a60e4784086
Yash Suhagiya <yashsuhagiya401@gmail.com>
2024-09-02 16:14:54 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+from dotenv import dotenv_values
+
+config = dotenv_values(".env")
+
+sp = spotipy.Spotify(
+    auth_manager=SpotifyOAuth(
+        client_id=config["SPOTIFY_CLIENT_ID"],
+        client_secret=config["SPOTIFY_CLIENT_SECRET"],
+        redirect_uri=config["SPOTIFY_REDIRECT_URI"],
+        scope="user-read-playback-state,user-read-currently-playing",
+    )
+)
+
+current_user = sp.current_user()
+
+assert current_user is not None
+
+print(current_user["id"], "token saved in '.cache' file.")
